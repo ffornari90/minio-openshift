73f508ab35df9ad789f38c9bd782bebbe159b8e6
Federico Fornari <federico.fornari@cnaf.infn.it>
2022-11-23 11:27:05 +0100

    separate api and console services

--- create-minio-route.sh
+++ create-minio-route.sh
@@ -1,2 +1,2 @@
-oc create route passthrough --namespace minio-ns --service=minio --port=minio-console
-oc create route passthrough --namespace minio-ns --service=minio-api --port=minio
+oc create route passthrough --namespace minio-ns --service=minio-console --port=minio-console
+oc create route passthrough --namespace minio-ns --service=minio-api --port=minio-api
